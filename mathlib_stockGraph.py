812c2f1010bbf901e9c612946750bacb5bf83ad5
Carlos Yoshio Tsuda <cyt231@gmail.com>
2018-01-10 13:18:36 -0200

    > Alteração da função "graph_data" >Alteração da variavel tipo > temp (19-38) >Modificação da variavel URL, melhora na funcionalidade do sistema >>Adição das funções: "INTRADAY", "DAILY ","WEEKLY", "MONTHLY" >Correção de um erro que não permitia utilizar a função "INTRADAY" da Alphavantage

--- mathlib_stockGraph.py
+++ mathlib_stockGraph.py
@@ -11,24 +11,33 @@ def bdate2num(fmt, encoding='utf-8'):
         return str_converter(s)
     return bconverter
 
-def graph_data (stock, api, tipo):
+def graph_data (stock, api):
 
     #Informações basicas
     base = 'https://www.alphavantage.co/query?' #Base da Query
 
     #Data type do API Alphavantage - Tipo (valores temporais)
-    if tipo == '1':
+    temp = input('Temporal Resolution: ')
+    print('=====================')
+    size = ''
+    if temp == '1':
+        print('Intraday')
+        aux = input('Interval (1,5,15,30,60 min): ')
+        size = '&interval='+aux + 'min'
         function = 'function=TIME_SERIES_INTRADAY'
-    elif tipo == '2':
+    elif temp == '2':
+        print('Daily')
         function = 'function=TIME_SERIES_DAILY'
-    elif tipo == '3':
+    elif temp == '3':
+        print('Weekly')
         function = 'function=TIME_SERIES_WEEKLY'
-    elif tipo =='4':
+    elif temp =='4':
+        print('Monthly')
         function = 'function=TIME_SERIES_MONTHLY'
     else:
-        print(type(tipo))
-        print("Error: "+tipo)
-        
+        print(type(temp))
+        print("Error: "+temp)
+    print('=====================')
         
     #Stock - ticket do stock
     symbol = '&symbol='+stock
@@ -39,9 +48,9 @@ def graph_data (stock, api, tipo):
 
     #Exibir informações
     print('Currently pulling: ', stock)   
-    url = base+function+symbol+'&outputsize=full&&apikey='+api+'&datatype=csv'
-    print('URL: ', url)
-
+    url = base+function+symbol+size+'&outputsize=full&apikey='+api+'&datatype=csv'
+    print('URL: '+ url + '\n>> Decodificando URL')
+    
     #URL Decode - decodificando o URL
     source_code = urllib.request.urlopen(url).read().decode()
     stock_data = [] #Array DATA
@@ -53,7 +62,7 @@ def graph_data (stock, api, tipo):
         if len(split_line) == 6:
             if 'timestamp' not in each_line:
                 stock_data.append(each_line)
-
+             
     #Transferindo os dados para as variáveis
                 #date       > data específica
                 #openp   > valor das ações ao abrir o dia
@@ -61,8 +70,13 @@ def graph_data (stock, api, tipo):
                 #lowp      > menor valor que as ações atingiu no dia
                 #closep  > valor que as ações fecharam
                 #volume > a quantidade de ações movimentadas
-    date, openp, highp, lowp, closep, volume = np.loadtxt(stock_data, delimiter= ',', unpack=True,converters={0:bdate2num('%Y-%m-%d')})
 
+    if(temp != '1'):
+        date, openp, highp, lowp, closep, volume = np.loadtxt(stock_data, delimiter= ',', unpack=True,
+                                                              converters={0:bdate2num('%Y-%m-%d')})
+    elif(temp=='1'):
+        date, openp, highp, lowp, closep, volume = np.loadtxt(stock_data, delimiter= ',', unpack=True,
+                                                              converters={0:bdate2num('%Y-%m-%d %H:%M:%S')})
     plt.plot_date(date, closep, '-')
     for label in ax1.xaxis.get_ticklabels():
         label.set_rotation(45)
@@ -74,5 +88,4 @@ def graph_data (stock, api, tipo):
 api = 'XDESS96825BHFYQE'
 while(True):
     stock = input('Stock to plot: ')
-    tipo = input('Temporal: ')
-    graph_data(stock,api,tipo)
+    graph_data(stock,api)
