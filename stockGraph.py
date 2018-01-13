02840e869b1d0ca3ae87d04d28027c084abdcbb6
Carlos Yoshio Tsuda <cyt231@gmail.com>
2018-01-13 15:51:48 -0200

    >Remoção de alguns erros de input

--- stockGraph.py
+++ stockGraph.py
@@ -4,59 +4,148 @@ import urllib
 import numpy as np
 import datetime as dt
 
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+# bdate2num - function to convert the raw info to a usable
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 def bdate2num(fmt, encoding='utf-8'):
     str_converter = mdates.strpdate2num(fmt)
     def bconverter(b):
         s = b.decode(encoding)
         return str_converter(s)
     return bconverter
-    
-    
+
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+# Get Function - function to prepare the query for alphavantage API
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>  
 def getFunction(fun):
     outputsize= ''
     interval= ''
+    ### Function = 1 ### INTRADAY ###
     if(fun==1):
-        print('Function: Intraday') 
-        fun = 'TIME_SERIES_INTRADAY'        #REQ - value for query
-        stock = input('Stock: ')                          #REQ - stock value (ticket)
-        ##Interval
-        print('----------------------------------------------------------------------------------------------------------')
-        interval = input('Interval (1, 5, 15, 30, 60 min): ') #REQ (time)
-        ##Outputsize (OPT, only daily)
-        print('----------------------------------------------------------------------------------------------------------')
-        outputsize = input('Outputsize - compact/full (default:compact): ') #OPT
-        
+        print ('>>> Function: INTRADAY <<<')
+        ##Stock - REQ
+        stock = input('>>Stock: ')
+        fun = 'TIME_SERIES_INTRADAY'
+        title = 'Intraday - '+stock+' - '
+        ##Interval - REQ
+        print(120 *'-')
+        valid = 0
+        while not valid:
+            try:
+                interval = input ( '>>Interval (1, 5, 15, 30, 60 min): ')
+                if( interval == '1'):
+                    valid = 1
+                elif( interval == '5'):
+                    valid = 1
+                elif( interval == '15'):
+                    valid = 1
+                elif( interval == '30'):
+                    valid = 1
+                elif( interval == '60'):
+                    valid = 1
+                else:
+                    print('Input %s is not a valid value.' %interval)
+                    print(type(interval))
+            except ValueError as e:
+                print ("Input %s is not a valid integer." % e.args[0].split(": ")[1])
+        print(120*'-')
+        ##Outputsize - OPT - Daily Only
+        valid = 0
+        while not valid:
+            outputsize = input('>>Outputsize (compact or full): ')
+            if(outputsize == 'compact'):
+                valid = 1
+            elif( outputsize == 'full'):
+                valid = 1
+            elif( outputsize == ''):
+                valid = 1
+            else:
+                print('Invalid %s is not a valid value.' % outputsize)
+        title = title + 'Outputsize: ' 
+        if( outputsize != ''):
+            title = title + outputsize
+        else:
+            title = title + 'Default(compact)'
+            
+    ### Function = 2 ### DAILY ###
     elif(fun==2):
         print('Function: Daily')
         fun = 'TIME_SERIES_DAILY'
-        stock = input('Stock: ')
-        ##Outputsize (OPT, only daily)
-        outputsize = input('Outputsize - compact/full (default:compact): ') #OPT
+        title = 'Daily - '
+        stock = input('>>Stock: ')
+        title = title + stock + ' - '
+        ##Outputsize - OPT - Daily Only
+        valid = 0
+        while not valid:
+            outputsize = input('>>Outputsize (compact or full): ')
+            if(outputsize == 'compact'):
+                valid = 1
+            elif( outputsize == 'full'):
+                valid = 1
+            elif( outputsize == ''):
+                valid = 1
+            else:
+                print('Invalid %s is not a valid value.' % outputsize)
+        title = title + 'Outputsize: ' 
+        if( outputsize != ''):
+            title = title + outputsize
+        else:
+            title = title + 'Default(compact)'
     elif(fun==3):
         print('Function: Daily Adjusted')
         fun = 'TIME_SERIES_DAILY_ADJUSTED'
-        stock = input('Stock: ')
-        ##Outputsize (OPT, only daily)
-        outputsize = input('Outputsize - compact/full (default:compact): ') #OPT
+        title = 'Daily Adjusted - '
+        stock = input('>>Stock: ')
+        title = title + stock + ' - '
+        ##Outputsize - OPT - Daily Only
+        valid = 0
+        while not valid:
+            outputsize = input('>>Outputsize (compact or full): ')
+            if(outputsize == 'compact'):
+                valid = 1
+            elif( outputsize == 'full'):
+                valid = 1
+            elif( outputsize == ''):
+                valid = 1
+            else:
+                print('Invalid %s is not a valid value.' % outputsize)
+        title = title + 'Outputsize: ' 
+        if( outputsize != ''):
+            title = title + outputsize
+        else:
+            title = title + 'Default(compact)'
     elif(fun==4):
         print('Function: Weekly')
         fun ='TIME_SERIES_WEEKLY'
-        stock = input('Stock: ')
+        title = 'Weekly - '
+        stock = input('>>Stock: ')
+        title = title + stock
+        ylabel = 'Time - Weekly'
     elif(fun==5):
         print('Function: Weekly Adjusted')
         fun = 'TIME_SERIES_WEEKLY_ADJUSTED'
+        title = 'Weekly Adjusted - '
         stock = input('Stock: ')
+        title = title + stock
+        ylabel = 'Time - Weekly Adjusted'
     elif(fun==6):
         print('Function: Monthly')
         fun = 'TIME_SERIES_MONTHLY'
+        title = 'Monthly - '
         stock = input('Stock: ')
+        title = title + stock
+        ylabel = 'Time - Monthly'
     elif(fun==7):
         print('Function: Monthly Adjusted')
         fun = 'TIME_SERIES_MONTHLY_ADJUSTED'
+        title = 'Monthly Adjusted - '
         stock = input('Stock: ')
+        title = title + stock
+        ylabel = 'Time - Monthly Adjusted'
     elif(fun==8):
         print('Batch Stock Quotes')
         fun = 'BATCH_STOCK_QUOTES'
+        title = 'Batch Stock Quotes'
         a= []
         prompt = '>'
         print('Stocks: ')
@@ -65,37 +154,44 @@ def getFunction(fun):
             a.append(line)
             line = input(prompt)
         stock = ','.join(str(e) for e in a)
+        title = title + stock
+        
     
     print('----------------------------------------------------------------------------------------------------------')
-    return fun, stock, interval, outputsize
+    return fun, stock, interval, outputsize, title
 
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+# Get URL (url generator for alphavantage API)
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 def getUrl():
-    
     #Basic Information
     ##Query Base (REQ)
     base = 'https://www.alphavantage.co/query?'
-
-    print('=============================')
+    print(60 * '=')
     print('Options:\n1) Intraday\n2) Daily\n3) Daily Adjusted (Not Implemented)\n4) Weekly')
     print('5) Weekly Adjusted (Not Implemented)\n6) Monthly\n7) Monthly Adjusted (Not Implemented)\n8) Batch Stock Quotes')
     
     ##Get function
-    fun = False
-    while (fun == False):
-        print('----------------------------------------------------------------------------------------------------------')
-        fun = input('Select the function: ')
-        fun = int(fun)
-        if (fun == 3):
-            print('Function: ', fun ,' isn\'t a valid operation (yet)')
-            fun=False
-        elif(fun ==5):
-            print('Function: ',fun , ' isn\'t a valid operation (yet)')
-            fun=False
-        elif(fun==7):
-            print('Function: ',fun ,' isn\'t a valid operation (yet)')
-            fun=False
-        
-    aux, stock, inter, output = getFunction(fun)
+    valid = 0
+    while not valid:
+        try:
+            fun = int (input('>>Selec the function: '))
+            if (fun == 3):
+                print('Function: ', fun ,' isn\'t a valid operation (yet)')
+                fun=False
+            elif(fun ==5):
+                print('Function: ',fun , ' isn\'t a valid operation (yet)')
+                fun=False
+            elif(fun==7):
+                print('Function: ',fun ,' isn\'t a valid operation (yet)')
+                fun=False
+            else:
+                valid = 1
+        except ValueError as e:
+                print ("'%s' is not a valid integer." % e.args[0].split(": ")[1])
+
+    print(60 * '=')              
+    aux, stock, inter, output, head = getFunction(fun)
         
     print(aux, '>>>', stock)
     
@@ -128,12 +224,15 @@ def getUrl():
     api_key = '&apikey='+api
     
     url = base + function + symbol + interval+ outputsize + api_key + datatype
-    
+    title = head
     print('URL = '+url)
-    return url, fun
+    return url, fun, title
 
 
-def graph_data (url, fun):
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+# Graph plot
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+def graph_data (url, fun, title):
     #Graph configuration 
     fig = plt.figure()
     ax1 = plt.subplot2grid((1,1), (0,0))
@@ -146,14 +245,13 @@ def graph_data (url, fun):
     stock_data = [] 
     #Dividir as linhas do API
     split_source = source_code.split('\n')
-
     #Recuperar as informações do arquivo CSV
     for each_line in split_source:
         split_line = each_line.split(',')
         if len(split_line) == 6:
             if 'timestamp' not in each_line:
                 stock_data.append(each_line)
-             
+
     #Transferindo os dados para as variáveis
                 #date       > data específica
                 #openp   > valor das ações ao abrir o dia
@@ -161,6 +259,7 @@ def graph_data (url, fun):
                 #lowp      > menor valor que as ações atingiu no dia
                 #closep  > valor que as ações fecharam
                 #volume > a quantidade de ações movimentadas
+            
     if(fun != 1):
         date, openp, highp, lowp, closep, volume = np.loadtxt(stock_data, delimiter= ',', unpack=True,
                                                               converters={0:bdate2num('%Y-%m-%d')})
@@ -170,13 +269,17 @@ def graph_data (url, fun):
     plt.plot_date(date, closep, '-')
     for label in ax1.xaxis.get_ticklabels():
         label.set_rotation(45)
-        
+
+    plt.title(title)
+    plt.xlabel('Time')
+    plt.ylabel('Value')
     ax1.grid(True) #, color='g', linestyle='-', linewidth=3)
     plt.subplots_adjust(left = .15, bottom = .15, right = .95, top = .95, wspace = .2, hspace = .2)
     plt.show()
 
-
-#Main code
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+#Código Principal
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 while(True):
-    url, fun = getUrl()
-    graph_data(url, fun)
+    url, fun, title = getUrl()
+    graph_data(url, fun, title)
